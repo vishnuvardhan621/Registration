d616446fb8da1034749fe688efbe964d3375521b
vishnuvardhan621 <77157239+vishnuvardhan621@users.noreply.github.com>
2021-01-08 22:21:10 +0530

    Registration

---
+++ src/RegisterDao.java
@@ -0,0 +1,58 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class RegisterDao
+{
+private String dburl="jdbc:mysql://localhost:3306/test";
+private String dbuname="root";
+private String dbpassword="srinith";
+private String dbdriver="com.mysql.cj.jdbc.Driver";
+
+public void loadDriver(String dbDriver)
+{
+try {
+Class.forName(dbDriver);
+} catch (ClassNotFoundException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+}
+
+public Connection getConnection()
+{
+Connection con=null;
+try {
+con=DriverManager.getConnection(dburl, dbuname, dbpassword);
+} 
+catch (SQLException e) 
+{
+e.printStackTrace( );
+}
+return con;
+}
+public String insert(Member member)
+{
+loadDriver(dbdriver);
+Connection con=getConnection();
+String result="Data entered successfully";
+String sql="insert into test.employee values(?, ?, ?,?)";
+try {
+PreparedStatement ps=con.prepareStatement(sql);
+
+ps.setString(1, member.getUname());
+ps.setString(2, member.getPassword());
+ps.setString(3, member.getEmail());
+ps.setString(4, member.getPhone() );
+ps.executeUpdate( );
+
+} catch (SQLException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+result="Data not entered";
+}
+return result;
+}
+}
